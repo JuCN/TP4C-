31b846edc428b435f9c6b24ddbd8889e9c1a8be0
shikshak <ft.mezidi@gmail.com>
2017-03-27 22:32:44 +0200

    version 2.1 avec livreurs consulte commande

--- GustatIF_B3431/src/main/java/vue/Main.java
+++ GustatIF_B3431/src/main/java/vue/Main.java
@@ -34,7 +34,7 @@ public class Main {
 
         JpaUtil.init();
 
-        //smetier.createLivreurs(); //a mettre que si pas de livreurs
+        smetier.createLivreurs(); //a mettre que si pas de livreurs
         System.out.println("Bienvenue sur l'application GustatIF ! ");
 
         choixIdentite();
@@ -81,23 +81,47 @@ public class Main {
             s = lireInteger("Qui êtes vous ? #Entrez le numero correspondant: ", l);
         }
         Livreur livreur = livreurs.get(s);
-        Integer f = null;
 
-        if (!livreur.getCommandes().isEmpty()) {
-            Commande coli = livreur.getCommandes().get(livreur.getCommandes().size() - 1);
-            if (coli.getEtat().equals("En cours")) {
-                while (f == null) {
-                    f = lireInteger("Avez vous fini cette commande ? oui(1)\n" + coli, Arrays.asList(0, 1));
-                }
-                if (f == 1) {
-                    smetier.finirCommande(livreur.getCommandes().get(livreur.getCommandes().size() - 1), livreur);
-                }
-            } else {
-                System.out.println("Vous n'avez aucune commande en cours...");
+        Integer t = null;
+
+        while (t != 0) {
+            t = lireInteger("Souhaitez vous cloturer votre commande(1) ou consulter votre historique des commandes(2)  ou quitter (0) ?\n", Arrays.asList(0, 1, 2));
+
+            switch (t) {
+
+                case 1:
+                    Integer f = null;
+                    if (!livreur.getCommandes().isEmpty()) {
+
+                        Commande coli = livreur.getCommandes().get(livreur.getCommandes().size() - 1);
+                        if (coli.getEtat().equals("En cours")) {
+                            while (f == null) {
+                                f = lireInteger("Avez vous fini cette commande ? oui(1)\n" + coli, Arrays.asList(0, 1));
+                            }
+                            if (f == 1) {
+                                smetier.finirCommande(livreur.getCommandes().get(livreur.getCommandes().size() - 1), livreur);
+                            }
+                        } else {
+                            System.out.println("Vous n'avez aucune commande en cours...");
+                        }
+
+                    } else {
+                        System.out.println("Vous n'avez aucune commande...");
+                    }
+
+                    break;
+
+                case 2:
+                    List<Commande> commandes = smetier.getCommandesForLivreur(livreur);
+                    for (int i = 0; i < commandes.size(); i++) {
+                        if (commandes.get(i).getEtat().equals("En cours")) {
+                            System.out.println("Clients : " + commandes.get(i).getClient() + " Restaurant : " + commandes.get(i).getRestaurant()
+                                    + " heure de debut : " + commandes.get(i).getDateDeb());
+                        }
+                    }
+                    break;
             }
 
-        } else {
-            System.out.println("Vous n'avez aucune commande...");
         }
     }
 
@@ -107,70 +131,69 @@ public class Main {
 
         Integer i = null;
 
-        while (i == null || i!= 0) {
+        while (i == null || i != 0) {
             i = lireInteger("Voulez vous finir une commande d'un drone(1), voir la belle carte(2) ou quitter l'application(0) ?", Arrays.asList(0, 1, 2));
-        
 
-        switch (i) {
-            case 1:
-                List<Livreur> livreurs = smetier.getLivreurs();
-                List<Integer> l = new ArrayList();
-                for (int h = 0; h < livreurs.size(); h++) {
-                    if (livreurs.get(h) instanceof LivreurMachine) {
-                        System.out.println("(" + h + ")" + livreurs.get(h).getNom());
-                        l.add(h);
+            switch (i) {
+                case 1:
+                    List<Livreur> livreurs = smetier.getLivreurs();
+                    List<Integer> l = new ArrayList();
+                    for (int h = 0; h < livreurs.size(); h++) {
+                        if (livreurs.get(h) instanceof LivreurMachine) {
+                            System.out.println("(" + h + ")" + livreurs.get(h).getNom());
+                            l.add(h);
+                        }
                     }
-                }
-                Integer s = null;
-                while (s == null) {
-                    s = lireInteger("Quel drone ? #Entrez le numero correspondant: ", l);
-                }
-                Livreur livreur = livreurs.get(s);
-                Integer f = null;
+                    Integer s = null;
+                    while (s == null) {
+                        s = lireInteger("Quel drone ? #Entrez le numero correspondant: ", l);
+                    }
+                    Livreur livreur = livreurs.get(s);
+                    Integer f = null;
 
-                if (!livreur.getCommandes().isEmpty()) {
-                    Commande coli = livreur.getCommandes().get(livreur.getCommandes().size() - 1);
-                    if (coli.getEtat().equals("En cours")) {
-                        while (f == null) {
-                            f = lireInteger("Avez vous fini cette commande ? oui(1)\n" + coli, Arrays.asList(0, 1));
+                    if (!livreur.getCommandes().isEmpty()) {
+                        Commande coli = livreur.getCommandes().get(livreur.getCommandes().size() - 1);
+                        if (coli.getEtat().equals("En cours")) {
+                            while (f == null) {
+                                f = lireInteger("Avez vous fini cette commande ? oui(1)\n" + coli, Arrays.asList(0, 1));
+                            }
+                            if (f == 1) {
+                                smetier.finirCommande(livreur.getCommandes().get(livreur.getCommandes().size() - 1), livreur);
+                            }
+                        } else {
+                            System.out.println("Vous n'avez aucune commande en cours...");
                         }
-                        if (f == 1) {
-                            smetier.finirCommande(livreur.getCommandes().get(livreur.getCommandes().size() - 1), livreur);
-                        }
+
                     } else {
-                        System.out.println("Vous n'avez aucune commande en cours...");
+                        System.out.println("Vous n'avez aucune commande...");
                     }
-
-                } else {
-                    System.out.println("Vous n'avez aucune commande...");
-                }
-                i = null;
-                break;
-            case 2:
-                i = null;
-                while (i == null || i != 0) {
-                    i = lireInteger("Souhaitez vous voir tous les restaurants (1), livreurs (2),\n "
-                            + "clients (3), livraisons en cours (4) ou quitter (0) ?", Arrays.asList(0, 1, 2, 3, 4));
-
-                    switch (i) {
-                        case 1:
-                            visualisationRestaurants();
-                            break;
-                        case 2:
-                            visualisationLivreurs();
-                            break;
-                        case 3:
-                            visualisationClients();
-                            break;
-                        case 4:
-                            visualisationCommandesEnCours();
-                            break;
-                        default:
-                            break;
-                    }
+                    i = null;
                     break;
-                }
-        }
+                case 2:
+                    i = null;
+                    while (i == null || i != 0) {
+                        i = lireInteger("Souhaitez vous voir tous les restaurants (1), livreurs (2),\n "
+                                + "clients (3), livraisons en cours (4) ou quitter (0) ?", Arrays.asList(0, 1, 2, 3, 4));
+
+                        switch (i) {
+                            case 1:
+                                visualisationRestaurants();
+                                break;
+                            case 2:
+                                visualisationLivreurs();
+                                break;
+                            case 3:
+                                visualisationClients();
+                                break;
+                            case 4:
+                                visualisationCommandesEnCours();
+                                break;
+                            default:
+                                break;
+                        }
+                        break;
+                    }
+            }
         }
     }
 
@@ -317,7 +340,7 @@ public class Main {
             p = lireInteger("Tapez le numero du produit que vous souhaitez.", l);
         }
         quantite = lireInteger("Quelle quantité ?");
-        Qte_Commande qcommande = new Qte_Commande( produits.get(p - 1), quantite);
+        Qte_Commande qcommande = new Qte_Commande(produits.get(p - 1), quantite);
         //smetier.createQteCommande(qcommande);
         return qcommande;
     }
